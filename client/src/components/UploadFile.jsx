aba000d7dac0005da9016a6f886fd7ee4dd34ac6
mayur-chace <mayur.kamthe@lockrfinance.com>
2023-01-28 12:35:23 +0530

    upload

--- client/src/components/UploadFile.jsx
+++ client/src/components/UploadFile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import {Button, Stack} from '@mui/material'
+import {Button, Stack, Switch, TextField} from '@mui/material'
 import {LoadingButton} from '@mui/lab'
 import Navbar from './Navbar';
 import { ToastContainer, toast } from 'react-toastify';
@@ -9,7 +9,9 @@ import 'react-toastify/dist/ReactToastify.css';
 function UploadFile() {
     const [file, setFile] = useState();
     const [fileName, setFileName] = useState('');
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [isJson, setIsJson] = useState(false)
+    const [jsonData, setJsonData] = useState('')
     const uploadFile = async (e) => {
         setIsLoading(false);
         if (file) {
@@ -20,6 +22,7 @@ function UploadFile() {
           try {
             if (fileFormData) {
               const upload = await UploadFileBuffer(fileFormData);
+              if(upload){
                 toast.success('🦄 Upload Successful!', {
                     position: "bottom-center",
                     autoClose: 5000,
@@ -30,6 +33,19 @@ function UploadFile() {
                     progress: undefined,
                     theme: "light",
                     });
+              }
+              else{
+                toast.error('👁 Upload Failed!', {
+                  position: "bottom-center",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                  });
+              }
                 setFileName("");
                 setFile()
                 setIsLoading(false);
@@ -54,18 +70,85 @@ function UploadFile() {
       }
 
       const UploadFileBuffer = async(formData) => {
-        console.log(process.env.REACT_APP_BASE_URL)
-        await fetch(`${process.env.REACT_APP_BASE_URL}`,{
+        const upload = await fetch(`${process.env.REACT_APP_BASE_URL}`,{
             method:'POST',
             body:formData
         })
+        if(upload.status === 200){
+          return true
+        }
+        else{
+          return false
+        }
+      }
+
+      const sendFile = async() => {
+        try {
+          setIsLoading(true);
+          const response = await fetch(`${process.env.REACT_APP_BASE_URL}/json`,{
+            method:'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: jsonData
+        });
+        if(response.status < 300){
+          toast.success('🦄 Data Sent!', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+        }
+        else{
+          toast.error(`👁 ${response.statusText}`, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+        }
+          setIsLoading(false);
+          setJsonData('')
+        } catch (error) {
+          toast.error('👁 Something went wrong!', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            setIsLoading(false);
+            setJsonData('')
+        }
       }
   return (
       <>
       <Stack>
         <Navbar/>
+        <div>
+        <label>JSON</label>
+        <Switch
+            checked={isJson}
+            onChange={() => setIsJson(!isJson)}
+            name="loading"
+            color="primary"
+          />
+          <label>Video / JSON Files</label>
+          </div>
         <div className='verticalBox' style={{width:'50%', margin:'auto', alignItems:'center', marginTop:'40vh'}}>
-          <div className="flexBox">
+          {isJson ? <div className="flexBox">
               <div className="upload-btn-wrapper">
                   <Button
                       // isLoading={isLoading && receiptFileName}
@@ -86,13 +169,20 @@ function UploadFile() {
               <p className="truncate">{fileName}</p>
           <button onClick={() => { setFile(); setFileName("") }} className="regulerButton">X</button>
           </Stack></div>}
-          </div>
+          </div>: 
           <div>
-              {isLoading ? <LoadingButton loading loadingIndicator="Uploading…" variant="contained">Fetch data</LoadingButton> : <Button variant="contained" onClick={uploadFile}>
-                  Upload
+            <TextField
+          id="outlined-error"
+          label="JSON Data" value={jsonData} onChange={(e)=>{setJsonData(e.target.value)}}/>
+          </div>
+          }
+          <div>
+              {isLoading ? <LoadingButton loading loadingIndicator= {isJson ? "Uploading…" : "Sending..."} variant="contained">Fetch data</LoadingButton> : <Button variant="contained" onClick={isJson ? uploadFile : sendFile}>
+              {isJson ? 'Upload' : 'Send'}
               </Button>}
           </div>
           </div>
+          
           </Stack>
           <ToastContainer
 position="bottom-center"
